78d78b0f67bf008641f855c5891ec11b29ff08c9
Suraj Senapati <surajsenapati58@gmail.com>
2025-04-11 21:56:12 +0530

    blog deleted

--- blog/backend/config/mongo-connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const mongooseURI = process.env.MONGODB_URI;
-
-mongoose.connect(mongooseURI)
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Could not connect to MongoDB', err));
-
-module.exports = mongoose.connection;
