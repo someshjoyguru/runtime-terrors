7857a99566d3b3cca51fc205e6bc635f6cacb907
Suraj Senapati <surajsenapati58@gmail.com>
2025-04-11 23:06:46 +0530

    mongo db

--- appointment_system/config/db.js
+++ appointment_system/config/db.js
@@ -3,10 +3,14 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/sp");
-    console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected: ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
-    console.log(`Mongodb Server Issue ${error}`.bgRed.white);
+    console.log(`MongoDB connection error: ${error}`.bgRed.white);
+    process.exit(1); // Optional: Exit if DB fails
   }
 };
 
